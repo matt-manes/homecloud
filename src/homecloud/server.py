25caccb44ff7fe7fe46885ac8a6165c863b00fa6
matt manes <mattmanes@pm.me>
2023-02-13 20:32:50 -0600

    refactor: refactor getting ip/port and starting server

--- src/homecloud/server.py
+++ src/homecloud/server.py
@@ -22,9 +22,20 @@ def ping(request: request_models.Request):
     return {"app_name": "$app_name", "host": request.host}
 
 
-if __name__ == "__main__":
+def get_serving_address() -> tuple[str, int]:
     print("Obtaining ip address...")
     ip = lanutils.get_myip()[0][0]
     print("Finding available port in range...")
     port = lanutils.get_available_port(ip, homecloud_utils.get_port_range())
-    os.system(f"uvicorn {Path(__file__).stem}:app --reload --host {ip} --port {port}")
+    return (ip, port)
+
+
+def start_server(uvicorn_args: list[str] = ["--reload"]):
+    ip, port = get_serving_address()
+    os.system(
+        f"uvicorn {Path(__file__).stem}:app {' '.join(uvicorn_args)} --host {ip} --port {port}"
+    )
+
+
+if __name__ == "__main__":
+    start_server()
