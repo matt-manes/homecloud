d13cdedd52a744d9b13a306153e4ff833b038e13
matt manes <mattmanes@pm.me>
2023-03-11 13:54:50 -0600

    feat: add pathlib import to template

--- src/homecloud/get_routes.py
+++ src/homecloud/get_routes.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 from fastapi import APIRouter
 
 from homecloud import homecloud_logging, request_models
+
+root = Path(__file__).parent
 
 router = APIRouter()
 logger = homecloud_logging.get_logger("$app_name_server")
